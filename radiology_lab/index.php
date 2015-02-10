0b0bf6d38b20f02e54fe85afbfd25cb4cb10ce93
sam9116 <sam.b5687@gmail.com>
2015-02-10 14:35:06 -0700

    initial commit

---
+++ radiology_lab/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+   <head>
+       <link rel="stylesheet" href="css/metro-bootstrap.css">
+       <script src="metro/min/jquery/jquery.min.js"></script>
+       <script src="metro/min/jquery/jquery.widget.min.js"></script>
+       <script src="metro/min/metro/metro.min.js"></script>
+   </head>
+   <body class="metro">
+        ... markup page ...
+    </body>
+</html>
+
