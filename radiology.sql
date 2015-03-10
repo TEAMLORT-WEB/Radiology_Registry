31810e60c1b4b1038f27af90e89e6bf8d94a9688
sam9116 <sam.b5687@gmail.com>
2015-03-09 18:35:41 -0600

    sql

---
+++ radiology.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 10, 2015 at 01:34 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `radiology`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_name` char(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `class` int(1) NOT NULL,
+  `person_id` int(50) NOT NULL,
+  `date_registered` date NOT NULL,
+  PRIMARY KEY (`person_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='stores the name, password, class of, person_id,and time registered for each user';
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_name`, `password`, `class`, `person_id`, `date_registered`) VALUES
+('sam9116', '123456789', 2, 123456, '2015-03-02');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
