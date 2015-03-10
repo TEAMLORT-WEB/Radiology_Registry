9760644f77da8aacbc241e7f72d316e663fb92a6
sam9116 <sam.b5687@gmail.com>
2015-03-09 18:11:14 -0600

    legit version

---
+++ account_login/login.php
@@ -0,0 +1,45 @@
+<?php
+
+session_start();
+
+$username = $_POST['login'];
+$password = $_POST['password'];
+$class = $_POST['class'];
+echo "$username";
+
+if($username&&$password)
+{
+    
+   $mysqli = new mysqli("localhost", "root", "goodtogo", "radiology");
+   
+   $query = mysqli_query($mysqli,"SELECT * FROM users WHERE user_name='$username'");
+   $numrows = mysqli_num_rows($query);
+   
+   if($numrows != 0)
+   {
+       while($row = mysqli_fetch_assoc($query))
+       {
+           $dbusername = $row['user_name'];
+           $dbpassword = $row['password'];
+           $dbtype=$row['class'];
+       }
+       if($username==$dbusername&&$password==$dbpassword)
+       {
+           
+           echo "Login successful,heading back to homepage";
+           header( "refresh:5; url=/home.php" ); 
+
+           $_SESSION['username']=$dbusername;
+           $_SESSION['usertype']=$class;
+
+       }
+       else
+           echo" Incorrect password";
+   }
+   else
+      die("That username doesn't exist");
+}
+else
+    die("Please enter a username");
+
+?>
