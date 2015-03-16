b82c2e965e8b2e81dff3178ea70ce934562f244d
SIVLEOL <ejlo@ualberta.ca>
2015-03-16 16:41:37 -0600

    Adding SQL file, note that locating images for records isn't done.

--- radiology.sql
+++ radiology.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 10, 2015 at 01:34 AM
+-- Generation Time: Mar 16, 2015 at 11:39 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -19,6 +19,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `radiology`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radiology_record`
+--
+
+CREATE TABLE IF NOT EXISTS `radiology_record` (
+  `record_id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `doctor_id` int(11) NOT NULL,
+  `radiologist_id` int(11) NOT NULL,
+  `test_type` varchar(100) NOT NULL,
+  `prescribing_date` date NOT NULL,
+  `test_date` date NOT NULL,
+  `diagnosis` varchar(1000) NOT NULL,
+  `description` varchar(4000) NOT NULL,
+  PRIMARY KEY (`record_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `radiology_record`
+--
+
+INSERT INTO `radiology_record` (`record_id`, `patient_id`, `doctor_id`, `radiologist_id`, `test_type`, `prescribing_date`, `test_date`, `diagnosis`, `description`) VALUES
+(1, 11, 101, 1001, 'blood test', '2015-03-16', '2015-03-18', 'normal', 'Everything appears normal.'),
+(2, 12, 102, 1002, 'blood test', '2015-03-17', '2015-03-18', 'normal', 'Only a bit low on vitamin D.');
 
 -- --------------------------------------------------------
 
@@ -40,7 +67,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`user_name`, `password`, `class`, `person_id`, `date_registered`) VALUES
-('sam9116', '123456789', 2, 123456, '2015-03-02');
+('sam9116', '123456789', 2, 123456, '2015-03-02'),
+('admin', 'admin', 3, 1, '2015-03-16');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- search_backend.php
+++ search_backend.php
@@ -52,10 +52,39 @@
                 
             } else if ($_SESSION['class'] == 1){
             //doctor can only view records of their patients
-                $query = mysqli_query($mysqli,"select * 
-                                               from radiology_record
-                                               WHERE CONTAINS(description, your key word) AND
-                                                     record_id IN (SELECT");
+                for ($i = 0; $i < count($terms); $i++){
+                    $result = mysqli_query($mysqli,"select * 
+                                                   from radiology_record
+                                                   WHERE (`record_id` LIKE '%".$terms[$i]."%' OR
+                                                         `patient_id` LIKE '%".$terms[$i]."%' OR
+                                                         `doctor_id` LIKE '%".$terms[$i]."%' OR
+                                                         `radiologist_id` LIKE '%".$terms[$i]."%' OR
+                                                         `test_type` LIKE '%".$terms[$i]."%' OR
+                                                         `prescribing_date` LIKE '%".$terms[$i]."%' OR
+                                                         `test_date` LIKE '%".$terms[$i]."%' OR
+                                                         `diagnosis` LIKE '%".$terms[$i]."%' OR
+                                                         `description` LIKE '%".$terms[$i]."%') AND 
+                                                         ");
+                    if(isset($union_result)){
+                        $new_result = array();
+                        while ($row = $result->fetch_assoc()) {
+                            for ($i = 0; $i < count($union_result); $i++){
+                                if ($union_result[0]['record_id'] == $row['record_id']){
+                                    $new_result[] =  $row;  
+                                }
+                            }
+                        }
+                        $union_result = $new_result;
+                    } else {
+                        $union_result = mysqli_fetch_all($result,MYSQLI_ASSOC);
+                    }
+                }
+                
+                for ($i = 0; $i < count($union_result); $i++){
+                    foreach ($union_result[$i] as $key => $value) {
+                        echo " $key: $value";
+                    }
+                }
 
             } else if ($_SESSION['class'] == 2) {
             //patient can only view his/her own records
