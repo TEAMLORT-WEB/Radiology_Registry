196abaa909d4bcd06a5d2d4fa935a1565cb7b1a5
sam9116 <sam.b5687@gmail.com>
2015-03-19 17:20:48 -0600

    register module

--- home.php
+++ home.php
@@ -69,7 +69,7 @@ session_start();
                                       
                                 echo '<div class="tile">
                                         <div class ="tile-content icon button bg-darkPink fg-white" id="editusrinfo">
-                                            <i class = "icon-cog"></i>
+                                              <a href ="modifyinfo.php"><i class = "icon-cog"></i></a>
                                                 
                                         </div>
                                         <div class="tile-status"><span class ="name">Modify User Info</span></div>
@@ -97,50 +97,6 @@ session_start();
                </div>
            </div>
        </div>
-       
-       
-       
-       
-       
-       
-       <script  type="text/javascript" charset="UTF-8" >
-            $(function () {
-                $("#editusrinfo").on('click', function () {
-                    $.Dialog({
-                        shadow: true,
-                        overlay: false,
-                        draggable: true,
-                        icon: '<i class = "icon-cog"></i>',
-                        title: 'Draggable window',
-                        width: 'auto',
-                        padding: 10,
-                        content: 'This Window is draggable by caption.',
-                        onShow: function () {
-                            var content = '<form id="login-form-1" action="account/login.php/" method ="POST">' +
-                                    '<p>Login</p>' +
-                                    '<div class="input-control text"><input type="text" name="login"><button class="btn-clear"></button></div>' +
-                                    '<p>Password</p>' +
-                                    '<div class="input-control password"><input type="password" name="password"><button class="btn-reveal"></button></div>' +
-                                    '<div class="input-control checkbox"><p>Remember Me <input type="checkbox" name="c1" checked/><span class="check"></span></p></div>' +
-                                    '<div class="form-actions">' +
-                                    '<button class="button primary">Login to...</button>&nbsp;' +
-                                    '<button class="button warning" type="button"><a href="account/registration.php">Register</a></button>&nbsp;' +
-                                    '<button class="button" type="button" onclick="$.Dialog.close()">Cancel</button> ' +
-                                    '</div>' +
-                                    '</form>';
-
-                            $.Dialog.title("Edit User Info");
-                            $.Dialog.content(content);
-                        }
-
-                    });
-                });
-
-                
-            });
-            
-      
-        </script>
 
        </div>
     </body>
--- modifyinfo.php
+++ modifyinfo.php
@@ -1,96 +1,148 @@
-<?php 
-    
+<?php
+    //require "account_login/login.php";
+    Session_start();
+    $mysqli = new mysqli("localhost", "root", "goodtogo", "radiology");
+   // $insertquery = mysqli_query($mysqli,"INSERT INTO user VALUES ('$ID','$username','$encpassword','')");
+
+?>
 
 
-    ?>
 <html>
    <head>
        <link rel="stylesheet" href="metro/css/metro-bootstrap.css">
+       <link rel="stylesheet" href="metro/css/iconFont.min.css">
+       <link rel="stylesheet" href="metro/css/metro-bootstrap-responsive.css">
        <script src="metro/jquery/jquery.min.js"></script>
        <script src="metro/jquery/jquery.widget.min.js"></script>
        <script src="metro/min/metro.min.js"></script>
        <script src="metro/min/load-metro.js"></script>
-       <script src="metro/js/metro-calendar.js"></script>
-       <script src="metro/js/metro-datepicker.js"></script>
-       <script src="metro/js/metro-global.js"></script>
-       <script>
-
-            $("#datepicker").datepicker();
-
-
-
-       </script>
-
-
+       
    </head>
    <body class="metro">
        <div class="grid">
-
-           <div id="row0" class="row" >
-               <div class="span12">
-                   <div class="row">
-                       <div class ="span4"></div>
-                       <div class ="span4 offset2">
-                           <a href="index.html"><img src="Assets/AHS_Logo.jpg" alt="Alberta Health Services" height="100px" width ="auto"></a>
-                           <form name="search" method="POST" action="upload/upload_backend.php"  >
-                                    <label>Please enter Doctor_id</label>
-                                    <div  class="input-control text" >
-                                        <input type="text" name="doctor_id"/>
-                                    </div>
-                                    <label>Please enter Patient_id</label>
-                                    <div  class="input-control text" >
-                                        <input type="text" name="patient_id"/>
-                                    </div>
-                                    <label>Please enter Test Type</label>
-                                    <div class="input-control text" >
-                                        <input type="text" name="test_type"/>
-                                    </div>
-                                    <label>When was the test prescribed? YYYY-MM-DD </label>
-                                    
-                                    
-                                    
-                                    
-                                    
-                                        <div class="input-control text" data-role="datepicker" data-format="yyyy/m/d">
-                                            <input name="test_date_prescribe" type="text">
-                                            <button class="btn-date"></button>
-                                        </div>
+       
+       <div id="row0" class="row" >
+           <div class="span12">
+               <div class="row">
+                   <div class ="span4"></div>
+                   <div class ="span4 offset2">
+                       <a href="home.php"><img src="Assets/AHS_Logo.jpg" alt="Alberta Health Services" height="100px" width ="auto"></a>
+                       <?php 
+                       
+                            if (isset($_SESSION['username']) and isset($_SESSION['id']))
+                            {
+                                
+                            
                                
-
-                                    <br/> 
-                                    
-                                    
-                                    <label>When was the test completed? YYYY-MM-DD </label>
-                                    
-                                    
-                                    
-                                   
-                                        <div class="input-control text" data-role="datepicker" data-format="yyyy/m/d">
-                                            <input name="test_date_completed" type="text">
-                                            <button class="btn-date"></button>
+                                echo  "<h3><p>You're logged in as: </h3>".$_SESSION['username'];echo '</p></h3>';
+                                echo  "<h3><p>ID:".$_SESSION['id'];echo'</p></h3>';
+                               
+                                /***if($_SESSION['class']=='1')
+                                {
+                                    echo '<div class="tile" data-click="transform">
+                                            
+                                          </div>';
+                                }
+                                else if($_SESSION['class'] == '2')
+                                {
+                                    echo '<div class="tile" data-click="transform"></div>';
+                                }
+                                else if($_SESSION['class'] == '3')
+                                {
+                                    echo '<div class="tile" data-click="transform"></div>';
+                                }
+                                echo "<p></p>";***/
+                                echo '<div class="tile">
+                                        <div id= "register" class ="tile-content icon bg-darkbrown fg-white">
+                                            <i class = "icon-plus-2"></i>
+                                                
                                         </div>
-                                    
-                                    
-                                    
-                                    <label>What is the diagnosis?</label>
-                                    <div  class="input-control text" >
-                                        <input name="test_diagnosis"  type="text" />
-                                    </div>
-                                    <label>Describe the result?</label>
-                                    <div  class="input-control text" >
-                                        <input name="test_description" type="text" />
-                                    </div>
-                                <input name ="submit" type="submit" value="Submit" class="button primary"/>
-                                <br><br/>
-                                <input name="clear-btn" type="reset" class ="bg-darkRed fg-white" value="Clear"/>
-                            </form>
-                       </div>
-                       <div class ="span4"></div>
-                   </div>
+                                        <div class="tile-status"><span class ="name">Create New User</span></div>
+                                      </div>';
+                                      
+                                      
+                                 echo '<div class="tile">
+                                         <div class ="tile-content icon bg-pink fg-white">
+                                            <i class = "icon-wrench"></i>
+                                                 
+                                         </div>
+                                         <div class="tile-status"><span class ="name">Change Personal Info</span></div>
+                                       </div>';
+                                      
+                                 echo '<div class="tile" data-click="transform" >
+                                         <div class ="tile-content icon bg-red fg-white" href ="logout.php">
+                                            <a href ="logout.php"><i class = "icon-switch"></i></a>
+                                                 
+                                         </div>
+                                         <div class="tile-status"><span class ="name">Logout</span></div>
+                                       </div>';
+                                       
+                                
+                                
+                            
+                            }
+                            else
+                                header ("location:  index.html");
+                                
+                       ?>
+                       
+        
+                   <div class ="span4"></div>
                </div>
            </div>
-           
        </div>
-    </body>
-</html>
+       
+       
+       
+       
+       
+<script type="text/javascript" charset="UTF-8" >
+            $(function () {
+                $("#register").on('click', function () {
+                    $.Dialog({
+                        shadow: true,
+                        overlay: false,
+                        draggable: true,
+                        icon: '<span class="icon-plus-2"></span>',
+                        title: 'Draggable window',
+                        width: 'auto',
+                        padding: 10,
+                        content: 'This Window is draggable by caption.',
+                        onShow: function () {
+                            var strVar="";
+                                strVar += "<form id=\"form1\" method =\"post\" action =\"create_new_user.php\">   ";
+                                strVar += "   	<p>Username: <input name =\"user_name\" type=\"text\" \/><\/p> ";
+                                strVar += "    <p>Password: <input name =\"password\" type=\"password\" \/><\/p>";
+                                strVar += "    <p>Repeat Password: <input name =\"repeatpassword\" type=\"password\" \/><\/p>          ";
+                                strVar += "    <p>First Name: <input name =\"first_name\" type=\"text\" \/><\/p> ";
+                                strVar += "    <p>Last Name: <input name =\"last_name\" type=\"text\" \/><\/p> ";
+                                strVar += "    <p>Address: <input name =\"address\" type=\"text\" \/><\/p> ";
+                                strVar += "    <p>email: <input name =\"email\" type=\"text\" \/><\/p> ";
+                                strVar += "    <p>Username: <input name =\"phone number\" type=\"text\" \/><\/p> ";
+                                strVar += "    <p><input name =\"submit\" type=\"submit\"\/> <button class=\"button\" type=\"button\" onclick=\"$.Dialog.close()\">Cancel<\/button> <\/p>";
+                                strVar += "<\/form>";
+                                strVar += "";
 
+                            $.Dialog.title("Create New User");
+                            $.Dialog.content(strVar);
+                        }
+
+                    });
+                });
+
+                
+            });
+            
+      
+        </script>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
