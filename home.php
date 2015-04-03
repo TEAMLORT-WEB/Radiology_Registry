3da353d80842dfa2bf015db3c87a38dc821dc864
sam9116 <sam.b5687@gmail.com>
2015-04-03 16:07:37 -0600

    privledges set for home page

--- home.php
+++ home.php
@@ -37,40 +37,53 @@ session_start();
                                
                                 echo  "<h3><p>You're logged in as: </h3>".$_SESSION['username'];echo '</p></h3>';
                                 echo  "<h3><p>ID:".$_SESSION['id'];echo'</p></h3>';
-                               
-                                /***if($_SESSION['class']=='1')
-                                {
-                                    echo '<div class="tile" data-click="transform">
-                                            
-                                          </div>';
-                                }
-                                else if($_SESSION['class'] == '2')
-                                {
-                                    echo '<div class="tile" data-click="transform"></div>';
-                                }
-                                else if($_SESSION['class'] == '3')
-                                {
-                                    echo '<div class="tile" data-click="transform"></div>';
-                                }
-                                echo "<p></p>";***/
+                                
                                 echo '<div class="tile">
                                         <div class ="tile-content icon bg-lightBlue fg-white">
                                             <a href ="search.php"><i class = "icon-search"></i></a>
                                                 
                                         </div>
-                                        <div class="tile-status"><span class ="name">Search</span></div>
+                                        <div class="tile-status"><span class ="name">Search Records By Description</span></div>
                                       </div>';
-                                      
-                                      
-                                 echo '<div class="tile">
-                                         <div class ="tile-content icon bg-darkGreen fg-white">
-                                             <a href ="upload.php"><i class = "icon-file"></i></a>
-                                                 
-                                         </div>
-                                         <div class="tile-status"><span class ="name">Upload</span></div>
-                                       </div>';
-                                      
-                                      
+                               
+                                if($_SESSION['class'] == 'r' or $_SESSION['class']=='a')
+                                {
+                                    echo '<div class="tile" data-click="transform" >
+                                            <div class ="tile-content icon bg-Magenta fg-white" href ="image.php">
+                                               <a href ="image.php"><i class = "icon-image"></i></a>
+                                                    
+                                            </div>
+                                            <div class="tile-status"><span class ="name">Upload Pacs Image</span></div>
+                                          </div>';
+                                }
+                                if($_SESSION['class'] == 'd' or $_SESSION['class']=='a')
+                                {
+                                    echo '<div class="tile">
+                                            <div class ="tile-content icon bg-darkGreen fg-white">
+                                                <a href ="upload.php"><i class = "icon-file"></i></a>
+                                                    
+                                            </div>
+                                            <div class="tile-status"><span class ="name">Upload Records</span></div>
+                                          </div>';
+                                }
+                                if($_SESSION['class']=='a')
+                                {
+                                          
+                                    echo '<div class="tile" data-click="transform" >
+                                            <div class ="tile-content icon bg-lime fg-white" href ="report.php">
+                                               <a href ="report.php"><i class = "icon-clipboard-2"></i></a>
+                                                    
+                                            </div>
+                                            <div class="tile-status"><span class ="name">Generate Report</span></div>
+                                          </div>';
+                                    echo '<div class="tile" data-click="transform" >
+                                            <div class ="tile-content icon bg-mauve fg-white" href ="analysis.php">
+                                               <a href ="analysis.php"><i class = "icon-pie"></i></a>
+                                                    
+                                            </div>
+                                            <div class="tile-status"><span class ="name">Data analysis</span></div>
+                                          </div>';      
+                                }      
                                 echo '<div class="tile">
                                         <div class ="tile-content icon button bg-darkPink fg-white" id="editusrinfo">
                                               <a href ="modifyinfo.php"><i class = "icon-cog"></i></a>
@@ -78,34 +91,13 @@ session_start();
                                         </div>
                                         <div class="tile-status"><span class ="name">Modify User Info</span></div>
                                       </div>';                                    
-                                 echo '<div class="tile" data-click="transform" >
-                                         <div class ="tile-content icon bg-lime fg-white" href ="report.php">
-                                            <a href ="report.php"><i class = "icon-clipboard-2"></i></a>
-                                                 
-                                         </div>
-                                         <div class="tile-status"><span class ="name">Generate Report</span></div>
-                                       </div>';
-                                 echo '<div class="tile" data-click="transform" >
-                                         <div class ="tile-content icon bg-Magenta fg-white" href ="image.php">
-                                            <a href ="image.php"><i class = "icon-image"></i></a>
-                                                 
-                                         </div>
-                                         <div class="tile-status"><span class ="name">Upload Pacs Image</span></div>
-                                       </div>';
-                                 echo '<div class="tile" data-click="transform" >
-                                         <div class ="tile-content icon bg-mauve fg-white" href ="analysis.php">
-                                            <a href ="analysis.php"><i class = "icon-pie"></i></a>
-                                                 
-                                         </div>
-                                         <div class="tile-status"><span class ="name">Data analysis</span></div>
-                                       </div>';
-                                 echo '<div class="tile" data-click="transform" >
-                                        <div class ="tile-content icon bg-red fg-white" href ="logout.php">
-                                           <a href ="logout.php"><i class = "icon-switch"></i></a>
-                                                
-                                        </div>
-                                        <div class="tile-status"><span class ="name">Logout</span></div>
-                                      </div>';
+                                echo '<div class="tile" data-click="transform" >
+                                       <div class ="tile-content icon bg-red fg-white" href ="logout.php">
+                                          <a href ="logout.php"><i class = "icon-switch"></i></a>
+                                               
+                                       </div>
+                                       <div class="tile-status"><span class ="name">Logout</span></div>
+                                     </div>';
                             
                             }
                             else
